06d4a9be5d437b34ee8f41f1d65fed1d257568cc
npunshi <nikhilpunshi@gmail.com>
2020-10-07 05:28:16 +0500

    Tutorial 1 Done

--- Tutorial1/python_exercises.py
+++ Tutorial1/python_exercises.py
@@ -1,15 +1,12 @@
-"""
-Intro to python exercises shell code
-"""
 
 def is_odd(x):
     """
     returns True if x is odd and False otherwise
     """
     if (x % 2 == 0):
-        print("False")
+        print("false")
     else:
-        print("True")
+        print("true")
 
 
 def is_palindrome(word):
@@ -30,6 +27,11 @@ def only_odds(numlist):
 
     ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
     """
+    newList = []
+    for i in numlist:
+        if is_odd(i) == "true":
+            newList.append(i)
+    print(newList)
 
 
 def count_words(text):
@@ -45,4 +47,10 @@ def count_words(text):
         {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
         'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
     """
-    
+    wordic = {}
+    for word in text:
+        if word in wordic:
+            wordic[word] +=1
+        else:
+            wordic[word] = 1
+    return wordic
